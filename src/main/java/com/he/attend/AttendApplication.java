9e75161b76fb92189ec0a20991376e02d3eadcee
Ahhhnnn <36215241+Ahhhnnn@users.noreply.github.com>
2019-01-17 15:18:16 +0800

    提交

---
+++ src/main/java/com/he/attend/AttendApplication.java
@@ -0,0 +1,14 @@
+package com.he.attend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendApplication.class, args);
+	}
+
+}
+
